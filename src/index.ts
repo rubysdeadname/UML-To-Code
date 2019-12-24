121aaceabba115575c380dbb10626611ba0b6a23
Gabriel Barker <gabrielbarker@gmail.com>
2019-12-24 14:34:12 +0000

    Error check in the API

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Project from "./lib/Project";
 import SupportedLanguages from "./lib/SupportedLanguages";
 import { createInterface } from "readline";
+import { existsSync } from "fs";
 
 const rl = createInterface({
   input: process.stdin,
@@ -8,9 +9,20 @@ const rl = createInterface({
 });
 
 rl.question("What is the filepath of your CSV data? \n", (path: string) => {
+  if (!existsSync(path)) {
+    console.log("Could not find file");
+    rl.close();
+    return;
+  }
   rl.question("What language do you want to generate? \n", (language: string) => {
     const languageConstructor = SupportedLanguages.parse(language);
-    if (!languageConstructor) throw "That language is not currently supported";
+
+    if (!languageConstructor) {
+      console.log("That language is not currently supported");
+      rl.close();
+      return;
+    }
+
     const project = new Project(path, languageConstructor);
     project.createClasses();
     console.log("Classes created successfully");
